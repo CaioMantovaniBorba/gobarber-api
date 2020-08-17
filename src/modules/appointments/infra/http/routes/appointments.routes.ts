252a62b3750ffdeae45bde1b38e0806a488fa2ce
Caio Mantovani Borba <caiovmborba@gmail.com>
2020-08-17 20:10:20 -0300

    Dependency Inversion Principle

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
@@ -8,24 +7,26 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
+/*
 appointmentsRouter.get('/', async (req, res) => {
-  console.log(req.user);
-
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
 
   return res.json(appointments);
 });
+*/
 
 appointmentsRouter.post('/', async (req, res) => {
   const { provider_id, date } = req.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
