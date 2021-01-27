0028b7a3efc29faaee1ba73c236c3f9c64ffe7c3
Caio Mantovani Borba <caiovmborba@gmail.com>
2021-01-27 11:34:00 -0300

    List of providers with serialization

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -5,7 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import uploadConfig from '@config/upload';
+// import uploadConfig from '@config/upload';
 
 import { Exclude, Expose } from 'class-transformer';
 
@@ -33,21 +33,29 @@ class User {
   @UpdateDateColumn()
   updated_at: Date;
 
-  @Expose()
+  @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
-    if (!this.avatar) {
-      return null;
-    }
-
-    switch (uploadConfig.driver) {
-      case 'disk':
-        return `${process.env.APP_API_URL}/files/${this.avatar}`;
-      case 's3':
-        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
-      default:
-        return null;
-    }
+    return this.avatar
+      ? `${process.env.APP_API_URL}/files/${this.avatar}`
+      : null;
   }
 }
+
+// @Expose()
+// getAvatarUrl(): string | null {
+//   if (!this.avatar) {
+//     return null;
+//   }
+
+//   switch (uploadConfig.driver) {
+//     case 'disk':
+//       return `${process.env.APP_API_URL}/files/${this.avatar}`;
+//     case 's3':
+//       return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+//     default:
+//       return null;
+//   }
+// }
+// }
 
 export default User;
